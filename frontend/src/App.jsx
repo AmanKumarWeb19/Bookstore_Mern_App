568df28cd27ac9dac4b178ec9bd6b2fcd2e22e4e
Aman Kumar <akgupta0524@gmail.com>
2024-07-29 12:05:32 +0530

    update login and sign up

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -11,7 +11,7 @@ const App = () => {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/course" element={<Courses />} />
-        <Route path="/signup" element={<SignUp />} />
+        <Route path="/register" element={<SignUp />} />
       </Routes>
       </div>
     </>
--- frontend/src/components/SignUp.jsx
+++ frontend/src/components/SignUp.jsx
@@ -1,3 +1,80 @@
+// import React from "react";
+// import { Link } from "react-router-dom";
+// import Login from "./Login";
+
+// const SignUp = () => {
+//   return (
+//     <>
+//       <div className="flex h-screen items-center justify-center ">
+//         <div className="w-[600px]">
+//           <div className="modal-box">
+//             <form method="dialog">
+//               {/* if there is a button in form, it will close the modal */}
+//               <Link
+//                 to="/"
+//                 className="btn btn-sm btn-circle btn-ghost absolute right-2 top-2"
+//               >
+//                 ✕
+//               </Link>
+//             </form>
+//             <h3 className="font-bold text-lg">Sign Up!</h3>
+//             {/* Name */}
+//             <div className="mt-4 space-y-2">
+//               <span>Name</span>
+//               <br />
+//               <input
+//                 type="text"
+//                 placeholder="Enter your Full Name"
+//                 className="w-80 px-3 py-1 border rounded-md outline-none"
+//               />
+//             </div>
+//             {/* Email  */}
+//             <div className="mt-4 space-y-2">
+//               <span>Email</span>
+//               <br />
+//               <input
+//                 type="email"
+//                 placeholder="Enter your Email"
+//                 className="w-80 px-3 py-1 border rounded-md outline-none"
+//               />
+//             </div>
+//             {/* Password */}
+//             <div className="mt-4 space-y-2">
+//               <span>Password!</span>
+//               <br />
+//               <input
+//                 type="password"
+//                 placeholder="Enter your Password"
+//                 className="w-80 px-3 py-1 border rounded-md outline-none"
+//               />
+//             </div>
+//             {/* Button */}
+//             <div className="flex justify-around mt-4">
+//               <button className="bg-pink-500 text-white rounded-md px-3 py-1 hover:bg-pink-700 duration-200">
+//                 SignUp
+//               </button>
+//               <p>
+//                 Have Account?{" "}
+//                 <button
+//                   className="text-blue-500 underline cursor-pointer"
+//                   onClick={() =>
+//                     document.getElementById("my_modal_3").showModal()
+//                   }
+//                 >
+//                   Login
+//                 </button>{" "}
+//                 <Login/>
+//               </p>
+//             </div>
+//           </div>
+//         </div>
+//       </div>
+//     </>
+//   );
+// };
+
+// export default SignUp;
+
 import React from "react";
 import { Link } from "react-router-dom";
 import Login from "./Login";
@@ -5,7 +82,7 @@ import Login from "./Login";
 const SignUp = () => {
   return (
     <>
-      <div className="flex h-screen items-center justify-center ">
+      <div className="flex h-screen items-center justify-center">
         <div className="w-[600px]">
           <div className="modal-box">
             <form method="dialog">
@@ -17,53 +94,52 @@ const SignUp = () => {
                 ✕
               </Link>
             </form>
-            <h3 className="font-bold text-lg">Sign Up!</h3>
-            {/* Name */}
+            <h3 className="font-bold text-lg">SignUp!</h3>
             <div className="mt-4 space-y-2">
-              <span>Name</span>
+              <span>Name:</span>
               <br />
               <input
                 type="text"
-                placeholder="Enter your Full Name"
-                className="w-80 px-3 py-1 border rounded-md outline-none"
+                placeholder="Enter Your Name"
+                className="w-80 px-3 py-1 rounded-md outline:none"
               />
             </div>
-            {/* Email  */}
+            {/* Email */}
             <div className="mt-4 space-y-2">
-              <span>Email</span>
+              <span>Email:</span>
               <br />
               <input
                 type="email"
-                placeholder="Enter your Email"
-                className="w-80 px-3 py-1 border rounded-md outline-none"
+                placeholder="Enter Your Email"
+                className="w-80 px-3 py-1 rounded-md outline:none"
               />
             </div>
-            {/* Password */}
+            {/* Password  */}
             <div className="mt-4 space-y-2">
-              <span>Password!</span>
+              <span>Password:</span>
               <br />
               <input
                 type="password"
-                placeholder="Enter your Password"
-                className="w-80 px-3 py-1 border rounded-md outline-none"
+                placeholder="Enter Your Password"
+                className="w-80 px-3 py-1 rounded-md outline:none"
               />
             </div>
-            {/* Button */}
+            {/* Login Button */}
             <div className="flex justify-around mt-4">
               <button className="bg-pink-500 text-white rounded-md px-3 py-1 hover:bg-pink-700 duration-200">
                 SignUp
               </button>
-              <p>
+              <p className="text-xl">
                 Have Account?{" "}
                 <button
-                  className="text-blue-500 underline cursor-pointer"
+                  className="underline text-blue-500 cursor-pointer"
                   onClick={() =>
                     document.getElementById("my_modal_3").showModal()
                   }
                 >
                   Login
-                </button>{" "}
-                <Login/>
+                </button>
+                <Login />
               </p>
             </div>
           </div>
