d89d864bb6bd0ea0fb9702345534fc3dcb06ae87
Aman Kumar <akgupta0524@gmail.com>
2024-07-30 12:52:38 +0530

    check and coreect the code

--- frontend/src/components/Login.jsx
+++ frontend/src/components/Login.jsx
@@ -1,80 +1,9 @@
-// import React from "react";
-// import { Link } from "react-router-dom";
-// import { useForm } from "react-hook-form"
-
-// const Login = () => {
-//     const {
-//         register,
-//         handleSubmit,
-//         formState: { errors },
-//       } = useForm()
-
-//       const onSubmit = (data) => console.log(data)
-//   return (
-//     <div>
-//       <dialog id="my_modal_3" className="modal">
-//         <div className="modal-box">
-//           <form  onSubmit={handleSubmit(onSubmit)} method="dialog">
-//             {/* if there is a button in form, it will close the modal */}
-//             <Link to="/" className="btn btn-sm btn-circle btn-ghost absolute right-2 top-2">
-//               ✕
-//             </Link>
-//           </form>
-//           <h3 className="font-bold text-lg">Login!</h3>
-//           {/* Email  */}
-//           <div className="mt-4 space-y-2">
-//             <span>Email</span>
-//             <br />
-//             <input
-//               type="email"
-//               placeholder="Enter your Email"
-//               className="w-80 px-3 py-1 border rounded-md outline-none"
-//               {...register("email", { required: true })}
-//             />
-//               {errors.email && <span>This field is required</span>}
-//           </div>
-//           {/* Password */}
-//           <div className="mt-4 space-y-2">
-//             <span>Password!</span>
-//             <br />
-//             <input
-//               type="password"
-//               placeholder="Enter your Password"
-//               className="w-80 px-3 py-1 border rounded-md outline-none"
-//               {...register("password", { required: true })}
-//             />
-//               {errors.password && <span>This field is required</span>}
-//           </div>
-//           {/* Button */}
-//           <div className="flex justify-around mt-4">
-//             <Link to="/" className="bg-pink-500 text-white rounded-md px-3 py-1 hover:bg-pink-700 duration-200">
-//               Login
-//             </Link>
-//             <p>
-//               Not Registered?{" "}
-//               <Link
-//                 to="/signUp"
-//                 className="text-blue-500 underline cursor-pointer"
-//               >
-//                 SignUp
-//               </Link>{" "}
-//             </p>
-//           </div>
-//         </div>
-//       </dialog>
-//     </div>
-//   );
-// };
-
-// export default Login;
-
 import React from "react";
 import { Link } from "react-router-dom";
 import { useForm } from "react-hook-form";
 import axios from "axios";
 import toast from "react-hot-toast";
-
-const Login = () => {
+function Login() {
   const {
     register,
     handleSubmit,
@@ -91,9 +20,8 @@ const Login = () => {
       .then((res) => {
         console.log(res.data);
         if (res.data) {
-          toast.success("Login Successful!");
+          toast.success("Loggedin Successfully");
           document.getElementById("my_modal_3").close();
-
           setTimeout(() => {
             window.location.reload();
             localStorage.setItem("Users", JSON.stringify(res.data.user));
@@ -103,7 +31,7 @@ const Login = () => {
       .catch((err) => {
         if (err.response) {
           console.log(err);
-          toast.error("error:-" + err.response.data.message);
+          toast.error("Error: " + err.response.data.message);
           setTimeout(() => {}, 2000);
         }
       });
@@ -125,12 +53,12 @@ const Login = () => {
             <h3 className="font-bold text-lg">Login!</h3>
             {/* Email */}
             <div className="mt-4 space-y-2">
-              <span>Email:</span>
+              <span>Email</span>
               <br />
               <input
                 type="email"
-                placeholder="Enter Your Email"
-                className="w-80 px-3 py-1 rounded-md outline:none"
+                placeholder="Enter your email"
+                className="w-80 px-3 py-1 border rounded-md outline-none"
                 {...register("email", { required: true })}
               />
               <br />
@@ -140,14 +68,14 @@ const Login = () => {
                 </span>
               )}
             </div>
-            {/* Password  */}
+            {/* password */}
             <div className="mt-4 space-y-2">
-              <span>Password:</span>
+              <span>Password</span>
               <br />
               <input
                 type="password"
-                placeholder="Enter Your Password"
-                className="w-80 px-3 py-1 rounded-md outline:none"
+                placeholder="Enter your password"
+                className="w-80 px-3 py-1 border rounded-md outline-none"
                 {...register("password", { required: true })}
               />
               <br />
@@ -157,19 +85,20 @@ const Login = () => {
                 </span>
               )}
             </div>
-            {/* Login Button */}
-            <div className="flex justify-around mt-4">
+
+            {/* Button */}
+            <div className="flex justify-around mt-6">
               <button className="bg-pink-500 text-white rounded-md px-3 py-1 hover:bg-pink-700 duration-200">
                 Login
               </button>
               <p>
-                Not Registered?{" "}
+                Not registered?{" "}
                 <Link
                   to="/register"
                   className="underline text-blue-500 cursor-pointer"
                 >
                   SignUp
-                </Link>
+                </Link>{" "}
               </p>
             </div>
           </form>
@@ -177,6 +106,6 @@ const Login = () => {
       </dialog>
     </div>
   );
-};
+}
 
 export default Login;
